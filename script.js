7490dbc70f35646cc6938817f8fc24b1c287f119
anilgpt738 <anilgpt738@gmail.com>
2026-09-12 10:54:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -920,7 +920,8 @@ accuracyElement.textContent = "100%";
 document.addEventListener("DOMContentLoaded", function () {
     const themeToggleButton = document.getElementById("theme-toggle");
     
-    const savedTheme = localStorage.getItem("theme") || "light";
+    // Default theme set to dark if no previous selection exists
+    const savedTheme = localStorage.getItem("theme") || "dark";
     
     document.documentElement.setAttribute("data-theme", savedTheme);
     if (themeToggleButton) {
